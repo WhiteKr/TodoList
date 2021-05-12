818a861bd7f64c7959550de16ef93f94474be891
WhiteKr <hanjisu2215g@gmail.com>
2021-05-12 10:13:41 +0900

    Create button function addTodo adding todo to list

---
+++ app.js
@@ -0,0 +1,34 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+// Functions
+
+function addTodo(event) {
+    // Prevent form from submitting
+    event.preventDefault();
+    // TODO DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // TODO LIST
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hi';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // CHECK BUTTON
+    const compeletedButton = document.createElement('button');
+    compeletedButton.innerHTML = '<i class="fas fa-check"></i>';
+    compeletedButton.classList.add('complete-btn');
+    todoDiv.appendChild(compeletedButton);
+    // TRASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // APPEND DIV TO LIST
+    todoList.appendChild(todoDiv);
+}
